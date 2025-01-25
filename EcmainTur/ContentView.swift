c36c127d9791bde5ed33193c1e4502a91bd1cfcc
Yavuz Selim <yavuz6661@gmail.com>
2025-01-25 22:24:37 +0300

    Initial Commit

---
+++ EcmainTur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EcmainTur
+//
+//  Created by Yavuz Selim Yılmaz on 25.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EcmainTur/EcmainTurApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcmainTurApp.swift
+//  EcmainTur
+//
+//  Created by Yavuz Selim Yılmaz on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct EcmainTurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
